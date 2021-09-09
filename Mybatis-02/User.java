1977681e955a0fd78beb5dd54179ab05fd23d1c6
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-09 22:05:41 +0800

    Create User.java

---
+++ Mybatis-02/User.java
@@ -0,0 +1,75 @@
+
+import org.apache.ibatis.type.Alias;
+
+import java.util.Objects;
+
+/**
+ * @author by wyl
+ * @date 2021/9/6.
+ */
+
+
+@Alias("user")
+//别名
+public class User {
+
+    private int id;
+    private String name;
+    private String pwd;
+
+    public User() {
+    }
+
+    public User(int id, String name, String pwd) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(pwd, user.pwd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, pwd);
+    }
+
+}
