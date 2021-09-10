114e178471e1bbd5c0097f7582322e2bfcb11b67
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-10 10:59:59 +0800

    Create UserMapper.java

---
+++ 分页/UserMapper.java
@@ -0,0 +1,35 @@
+
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * UserMapper 相当于UserDao接口  定义一些接口方法
+ *
+ * @author by wyl
+ * @date 2021/9/6.22点08分
+ */
+
+public interface UserMapper {
+
+    /**
+     * 得到全部用户集合
+     */
+    List<User> getUserList();
+
+
+    /**
+     * 分页得到全部用户集合
+     */
+    List<User> getUserListLimit(Map<String, Object> map);
+
+
+    /**
+     * 得到全部用户集合
+     */
+    List<User> getUserListRowBounds();
+  
+}
+
+
+
