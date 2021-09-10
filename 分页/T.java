4a18bc5c24f4aaca07f21329675ffa764a421a45
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-10 11:02:31 +0800

    Create T.java

---
+++ 分页/T.java
@@ -0,0 +1,57 @@
+
+
+
+
+import dao.UserMapper;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import pojo.User;
+import utils.MybatisUtils;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Mybatis_T
+ *
+ * @author by wyl
+ * @date 2021/9/9.11点02分
+ */
+
+public class T {
+
+    private static Logger logger = Logger.getLogger(T.class);
+
+    @Test
+    public void query_T() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        logger.info(sqlSession);
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = userMapper.getUserList();
+        userList.forEach((v) -> System.out.println(v));
+        sqlSession.close();
+    }
+
+    @Test
+    public void queryLimit_T() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("startIndex", 0);
+        map.put("pageSize", 2);
+        List<User> userListLimit = mapper.getUserListLimit(map);
+        userListLimit.forEach((v) -> System.out.println(v));
+        sqlSession.close();
+    }
+
+    @Test
+    public void queryRowBounds_T() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        List<User> userListRowBounds = sqlSession.selectList("dao.UserMapper.getUserListRowBounds",null,new RowBounds(1,2));
+        userListRowBounds.forEach((v) -> System.out.println(v));
+        sqlSession.close();
+    }
+
+}
