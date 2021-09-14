71c77493159742f1fe7c70be71c917beba2f12ad
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-14 11:13:01 +0800

    Create User.java

---
+++ annotation开发/User.java
@@ -0,0 +1,67 @@
+package pojo;
+
+import java.util.Objects;
+
+
+public class User {
+
+    private int id;
+    private String name;
+    private String pwd;
+
+
+    public User() {
+    }
+
+    public User(int id, String name, String pwd) {
+        this.id = id;
+        this.name = name;
+        this.pwd = pwd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(pwd, user.pwd);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, pwd);
+    }
+}
