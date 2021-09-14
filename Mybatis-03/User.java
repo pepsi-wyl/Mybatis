e06877b1a50f796ce81cbf3a369650c9b27fe145
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-14 11:00:23 +0800

    Create User.java

---
+++ Mybatis-03/User.java
@@ -0,0 +1,59 @@
+package pojo;
+
+import org.apache.ibatis.type.Alias;
+
+/**
+ * @author by wyl
+ * @date 2021/9/6.
+ */
+
+@Alias("user")
+public class User {
+
+    private int id;
+    private String name;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+}
