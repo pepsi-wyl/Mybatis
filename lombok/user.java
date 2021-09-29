b563d6a37f0c89ee2850b6d9c38b98ba992948b8
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-09-29 21:22:41 +0800

    Update user.java

--- lombok/user.java
+++ lombok/user.java
@@ -4,7 +4,7 @@ import lombok.*;
 
 @Data                    //无参构造  get set toString  hashCode equals
 @AllArgsConstructor      //有参构造
-@NoArgsConstructor       //无参构造
+@NoArgsConstructor       //无参构造      //该类有了有参构造器之后需显示声明无参构造器
 
 @ToString                //toString
 @EqualsAndHashCode       //equals hashCode
